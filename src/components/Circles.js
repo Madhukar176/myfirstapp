32635b6f6874fad9b543eacb554f3c9ffce1edf8
Madhukar176 <75468129+Madhukar176@users.noreply.github.com>
2020-12-05 14:19:36 +0530

    Update Circles.js

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -4,11 +4,9 @@ const Circles =(props)=>{
     return(
 
         <div style={{border:"2px solid black",height:"40px",width:"40px", borderRadius:"50px",
-        top:props.top,left:props.left,backgroundColor:"white", position:"fixed", zIndex:100}}></div>
+        top:props.top,left:props.left,backgroundColor:"white", position:"fixed", zIndex:1}}></div>
 
-        // <div style={{width:"50px",height:"50px",borderRadius:"25px",top:props.top,
-        // left:props.left,border:"1px solid black", zIndex:100,position:"absolute",
-        // backgroundColor:"white"}}></div>
+        
 
     )
 
